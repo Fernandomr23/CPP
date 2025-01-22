9bd777d5dc9c773a2f5e88a04a09f33a35ec5398
Fernando <fmorenilla23@gmail.com>
2025-01-22 11:23:06 +0100

    Arreglos Makefile para compilar objetos con c++ y 02 ex01 hecho

--- 02/ex01/Fixed.cpp
+++ 02/ex01/Fixed.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Fixed.cpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fmorenil <fmorenil@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fernando <fernando@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 10:57:05 by fmorenil          #+#    #+#             */
-/*   Updated: 2025/01/20 13:21:51 by fmorenil         ###   ########.fr       */
+/*   Updated: 2025/01/22 11:14:38 by fernando         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,12 +27,30 @@ Fixed::Fixed(const Fixed& num) {
 
 Fixed::Fixed(const int num) {
 	std::cout << "Int constructor called" << std::endl;
-	this->_value = num; 
+	this->_value = num << bits; 
 }
 
 Fixed::Fixed(const float num) {
-	std::cout << "Int constructor called" << std::endl;
-	this->_value = num; 
+	std::cout << "Float constructor called" << std::endl;
+	this->_value = (int)roundf(num * (1 << bits)); 
+}
+
+float	Fixed::toFloat(void) const {
+	return ((float)this->_value / (1 << bits));
+}
+
+int	Fixed::toInt(void) const {
+	return (this->_value >> bits);
+}
+
+int Fixed::getRawBits(void) const {
+	// std::cout << "getRawBits member function called" << std::endl;
+	return (this->_value);
+}
+
+void Fixed::setRawBits(int const raw) {
+	std::cout << "setRawBits member function called" << std::endl;
+	this->_value = raw;
 }
 
 Fixed& Fixed::operator=(const Fixed& num) {
@@ -43,12 +61,7 @@ Fixed& Fixed::operator=(const Fixed& num) {
 	return (*this);
 }
 
-int Fixed::getRawBits(void) const {
-	std::cout << "getRawBits member function called" << std::endl;
-	return (this->_value);
-}
-
-void Fixed::setRawBits(int const raw) {
-	std::cout << "setRawBits member function called" << std::endl;
-	this->_value = raw;
+std::ostream& operator<<(std::ostream& os, const Fixed& num) {
+	os << num.toFloat();
+	return (os);
 }
--- 02/ex01/Fixed.hpp
+++ 02/ex01/Fixed.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Fixed.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fmorenil <fmorenil@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fernando <fernando@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 10:50:15 by fmorenil          #+#    #+#             */
-/*   Updated: 2025/01/20 13:15:28 by fmorenil         ###   ########.fr       */
+/*   Updated: 2025/01/22 11:13:00 by fernando         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,16 @@ public:
 	int		getRawBits(void) const;
 	void	setRawBits(int const raw);
 	float	toFloat(void) const;
-	float	toInt(void) const;
+	int		toInt(void) const;
 	Fixed();
 	Fixed(const Fixed& num);
 	Fixed(const int num);
 	Fixed(const float num);
 	Fixed& operator=(const Fixed& num);
-	Fixed& operator<<(const Fixed& num);
 	~Fixed();
 private:
 	int	_value;
 	static const int bits = 8;	
 };
+
+std::ostream& operator<<(std::ostream& os, const Fixed& num);
