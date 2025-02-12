2cde975b8e8fdc399830dcbd2e10636b5a9cb806
Fernando Morenilla Rivero <fmorenil@c2r17s6.42madrid.com>
2025-02-12 13:13:44 +0100

    Arreglos 04

--- 04/ex00/src/main.cpp
+++ 04/ex00/src/main.cpp
@@ -6,13 +6,13 @@
 /*   By: fmorenil <fmorenil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 19:52:23 by fmorenil          #+#    #+#             */
-/*   Updated: 2025/02/05 11:49:29 by fmorenil         ###   ########.fr       */
+/*   Updated: 2025/02/12 13:01:04 by fmorenil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <Dog.hpp>
 #include <Cat.hpp>
-#include <WrongCat.hpp>
+#include <Animal.hpp>
 
 int main()
 {
